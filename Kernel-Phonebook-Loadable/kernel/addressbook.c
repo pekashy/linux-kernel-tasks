644bd5e39e616ae83894fc9077f0ddf02be9218f
Egor Zyazev <klever.kl@yandex.ru>
2020-09-28 02:05:12 +0300

    Add and Get users test passed

--- Kernel-Phonebook-Loadable/kernel/addressbook.c
+++ Kernel-Phonebook-Loadable/kernel/addressbook.c
@@ -6,6 +6,15 @@
 #include <linux/list.h>
 #include <linux/slab.h>
 
+
+#define DEVICE_NAME "addressbook"
+
+static int dev_open(struct inode*, struct file*);
+static int dev_release(struct inode*, struct file*);
+static ssize_t dev_read(struct file*, char*, size_t, loff_t*);
+static ssize_t dev_write(struct file*, const char*, size_t, loff_t*);
+
+
 typedef struct _user
 {
 	char* firstName;
@@ -15,6 +24,7 @@ typedef struct _user
 	char* phone;
 } BDUSER;
 
+BDUSER* gQueriedUser = NULL;
 
 void KFreeUser(BDUSER* pUser)
 {
@@ -156,69 +166,183 @@ int AddUser(BDUSER* pUser)
 	return 0;
 }
 
-#define DEVICE_NAME "addressbook"
+int ProcessAdd(const char* pBuffer)
+{
+	printk(KERN_INFO "Processing insertion of: '%s'\n", pBuffer);
+	char* safeString = kzalloc(strlen(pBuffer), GFP_KERNEL);
+	strcpy(safeString, pBuffer);
+	char *token = strsep(&safeString, " "); // ID of operation
+	printk(KERN_INFO "Op ID : '%s'\n", token);
 
-static int dev_open(struct inode*, struct file*);
-static int dev_release(struct inode*, struct file*);
-static ssize_t dev_read(struct file*, char*, size_t, loff_t*);
-static ssize_t dev_write(struct file*, const char*, size_t, loff_t*);
+   	const char* rFirstName = strsep(&safeString, " ");
+	if(rFirstName == NULL)
+	{
+		kzfree(safeString);
+		return -2;
+	}
+	printk(KERN_INFO ": '%s'\n", rFirstName);
+
+	const char* rLastName = strsep(&safeString, " ");
+	if(rLastName == NULL)	
+	{
+		kzfree(safeString);
+		return -2;
+	}
+
+	printk(KERN_INFO ": '%s'\n", rLastName);
+
+	const char* rAge = strsep(&safeString, " ");
+	if(rAge == NULL)
+	{
+		kzfree(safeString);
+		return -2;
+	}
+	printk(KERN_INFO ": '%s'\n", rAge);
+	unsigned long nAge;
+	int res = kstrtol(rAge, 10, &nAge);
+	if(res)
+	{
+		printk(KERN_INFO "Failed to parse number %s with code %d\n", rAge, res);
+		kzfree(safeString);
+		return res;
+	}
+	const char* rEmail = strsep(&safeString, " ");
+	if(rEmail == NULL)
+	{
+		kzfree(safeString);
+		return -2;
+	}
+	printk(KERN_INFO ": '%s'\n", rEmail);
+
+	const char* rPhone = strsep(&safeString, " ");
+	if(rPhone == NULL)
+	{
+		kzfree(safeString);
+		return -2;
+	}
+	printk(KERN_INFO ": '%s'\n", rPhone);
+
+	BDUSER userSpaceAddressedUser = 
+		{rFirstName,
+		 rLastName,
+		 nAge,
+		 rEmail,
+		 rPhone};
+	BDUSER* pPersistentUser = KCloneUser(&userSpaceAddressedUser);
+	kzfree(safeString);
+	return AddUser(pPersistentUser);
+}
+
+int ProcessGet(const char* pBuffer)
+{
+	printk(KERN_INFO "Processing querying of: '%s'\n", pBuffer);
+	char* safeString = kzalloc(strlen(pBuffer), GFP_KERNEL);
+	strcpy(safeString, pBuffer);
+	char *token = strsep(&safeString, " "); // ID of operation
+	printk(KERN_INFO "Op ID : '%s'\n", token);
+   	const char* rLastName = strsep(&safeString, " ");
+	if(rLastName == NULL)
+	{
+		kzfree(safeString);
+		return -2;
+	}
+	printk(KERN_INFO ": '%s'\n", rLastName);
+
+	gQueriedUser = GetUser(rLastName);
+	return gQueriedUser == NULL;
+}
+
+int ProcessDelete(const char* pBuffer)
+{
+	return 0;
+}
 
 static struct file_operations fops = {
-    .open = dev_open,
-    .read = dev_read,
-    .write = dev_write,
-    .release = dev_release,
+	.open = dev_open,
+	.read = dev_read,
+	.write = dev_write,
+	.release = dev_release,
 };
 
 static int major;
 
 static int __init addressbook_init(void)
 {
-    printk(KERN_INFO "Address Book module has been loaded %d\n", major);
-    major = register_chrdev(0, DEVICE_NAME, &fops);
-    if(major < 0)
-    {
-        printk(KERN_ALERT "Address Book load failed");
-    }
-    printk(KERN_INFO "Address Book module has been loaded %d\n", major);
-    return 0;
+	printk(KERN_INFO "Address Book module has been loaded %d\n", major);
+	major = register_chrdev(0, DEVICE_NAME, &fops);
+	if(major < 0)
+	{
+		printk(KERN_ALERT "Address Book load failed");
+	}
+	printk(KERN_INFO "Address Book module has been loaded %d\n", major);
+	return 0;
 }
 
 static void __exit addressbook_exit(void)
 {
-    unregister_chrdev(major, DEVICE_NAME);
-    printk(KERN_INFO "Address Book module has been unloaded\n");
-    return;
+	unregister_chrdev(major, DEVICE_NAME);
+	printk(KERN_INFO "Address Book module has been unloaded\n");
+	return;
 }
 
 static int dev_open(struct inode* pInode, struct file* pFile)
 {
-    printk(KERN_INFO "Address Book device opened\n");
-    return 0;
+	printk(KERN_INFO "Address Book device opened\n");
+	return 0;
 }
 
 static int dev_release(struct inode* pInode, struct file* pFile)
 {
-    printk(KERN_INFO "Address Book device closed\n");
-    return 0;
+	printk(KERN_INFO "Address Book device closed\n");
+	return 0;
 }
 
 static ssize_t dev_read(struct file* pFile, char* pBuffer, size_t len, loff_t* offset)
 {
-    printk(KERN_ERR "Read attempt\n");
-    //BDUSER* user = GetUser("blah blah blah");
-    int errors = 0;
-    char* pMessage = "never gonna give you up...";
-    int message_len = strlen(pMessage);
-    errors = copy_to_user(pBuffer, pMessage, message_len);
+	printk(KERN_ERR "Read attempt\n");
+	int errors = 0;
+	if(gQueriedUser == NULL)
+	{
+		return 0;
+	}
 
-    return errors == 0 ? message_len : -EFAULT;
+	char* firstName;
+	char* lastName;
+	int age;
+	char* email;
+	char* phone;
+
+
+	snprintf(pBuffer, len, "%s %s %d %s %s\0",
+			gQueriedUser->firstName, gQueriedUser->lastName, gQueriedUser->age, gQueriedUser->email, gQueriedUser->phone);
+	printk("%s\n", pBuffer);
+	return errors == 0 ? strlen(pBuffer) : 0;
 }
 
 static ssize_t dev_write(struct file* pFile, const char* pBuffer, size_t len, loff_t* offset)
 {
-    printk(KERN_ERR "Now read-only\n");
-    return -EFAULT;
+	if(strlen(pBuffer) < 3)
+	{
+		return -EFAULT;
+	}
+	int errors = 0;
+	switch (*pBuffer)
+	{
+	case '1':
+		errors = ProcessAdd(pBuffer);
+		break;
+	case '2':
+		errors = ProcessGet(pBuffer);
+		break;
+	case '3':
+		errors = ProcessDelete(pBuffer);
+		break;
+	default:
+		errors = -2;
+		break;
+	}
+	printk(KERN_ERR "Now read-only\n");
+	return errors == 0 ? strlen(pBuffer) : -EFAULT;
 }
 
 module_init(addressbook_init);
--- Kernel-Phonebook-Loadable/test_loadable.c
+++ Kernel-Phonebook-Loadable/test_loadable.c
@@ -9,7 +9,6 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-// Open the device in non-blocking mode
 
 void test_read()
 {
@@ -17,13 +16,13 @@ void test_read()
     if(fd < 0)
     {
 		printf("Opening device for read failed: %d\n", fd);
-        assert(0);  // handle error
+        assert(0);
     }
 	char* buffer = malloc(256);
 	int rd = read(fd, buffer, 250);
 	buffer[rd] = 0;
 	assert(rd);
-	printf("Test read succeeded!: %s\n", buffer);
+	printf("Test <Read> succeeded!: %s\n", buffer);
 	free(buffer);
 	close(fd);
 }
@@ -34,35 +33,85 @@ void test_write()
     if(fd < 0)
     {
 		printf("Opening device for write failed: %d\n", fd);
-        assert(0);  // handle error
+        assert(0);
     }
 	char* buffer = malloc(256);
 	int wr = write(fd, buffer, 250);
 	assert(wr != -1);
-	//printf("Test write succeeded!: %s\n",);
+	printf("Test <Write> succeeded!\n");
 	free(buffer);
 	close(fd);
 }
 
-/*
-void test_write()
+void testAdd()
 {
-    // Try to write some data
-    ssize_t written = write(fd, "data", 4);
-    if(written >= 0)
-        ;  // handle successful write (which might be a partial write!)
-    else if(errno == EWOULDBLOCK)
-        ;  // handle case where the write would block
-    else
-        ;  // handle real error
+    int fd = open("/dev/addressbook", O_RDWR | O_NONBLOCK);
+    if(fd < 0)
+    {
+		printf("Opening device for write failed: %d\n", fd);
+        assert(0);
+    }
+    char* buffer = "1 John Wick 37 babayaga@bb.cc 11-22-33";
+    int wr = write(fd, buffer, strlen(buffer));
+	assert(wr == strlen(buffer));
+	printf("Test <Add User> succeeded!\n");
+    close(fd);
+}
 
-    // Reading data is similar
+void testDelete()
+{
 
 }
-*/
+
+void testGet()
+{
+    int fd = open("/dev/addressbook", O_RDWR | O_NONBLOCK);
+    if(fd < 0)
+    {
+		printf("Opening device for write failed: %d\n", fd);
+        assert(0);
+    }
+    char* buffer = "1 John Wick2 38 babayaga@bb.cc 11-22-33";
+    int wr = write(fd, buffer, strlen(buffer));
+	assert(wr == strlen(buffer));
+	
+    char* buffer3 = "2 Wick3";
+    wr = write(fd, buffer3, strlen(buffer3));
+	assert(wr != strlen(buffer3));
+
+    char* buffer2 = "2 Wick2";
+    wr = write(fd, buffer2, strlen(buffer2));
+	assert(wr == strlen(buffer2));
+    
+    //Now ready to read user
+    close(fd);
+    fd = open("/dev/addressbook", O_RDONLY | O_NONBLOCK);
+    if(fd < 0)
+    {
+		printf("Opening device for write failed: %d\n", fd);
+        assert(0);
+    }
+	char* buffer4 = malloc(4096);
+	int rd = read(fd, buffer4, 4096);
+	buffer4[rd] = 0;
+	assert(rd);
+    assert(!strcmp(buffer4, "John Wick2 38 babayaga@bb.cc 11-22-33"));
+    free(buffer4);
+    close(rd);
+    printf("Test <Add-Get User> succeeded!\n");
+}
+
+void test()
+{
+    //test_read();
+	//test_write();
+    testAdd();
+    testGet();
+}
+
+
 int main()
 {
     printf("Loadable!\n");
-	test_read();
-	test_write();
+    test();
 }
